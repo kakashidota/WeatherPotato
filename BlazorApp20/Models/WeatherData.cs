d324adc7fa234fcaa86dee91ac06846317847a52
Robin Kamo <robin.kamo@iths.se>
2024-09-02 14:57:00 +0200

    Add project files.

---
+++ BlazorApp20/Models/WeatherData.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp20.Models
+{
+    public class WeatherData
+    {
+        public Main Main { get; set; }
+        public Weather[] Weather { get; set; }
+    }
+
+    public class Main
+    {
+        public double Temp { get; set; }
+    }
+
+    public class Weather
+    {
+        public string Description { get; set; }
+    }
+}
